e323e4ef08cf58d58e8b79196daf97fe24cd631e
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-03-02 19:48:48 -0500

    fix: config updated / avatar uses astro Image instead of HTML img

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,10 +3,10 @@ export const SITE_TITLE = "Links Page";
 export const SITE_DESCRIPTION = "Links Page for Social Media";
 
 // profile
-export const PROFILE_NAME = "Vargalka Elswart";
-export const PROFILE_DESC =
-  "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda.";
-export const AVATAR_IMG = "/avatar.webp";
+export const PROFILE_NAME = "Anderson Marques";
+export const PROFILE_TITLE = "Software Engineer";
+export const AVATAR_IMG =
+  "https://avatars.githubusercontent.com/u/78233612?v=4";
 
 // social icons
 export const SOCIAL_ICONS = [
